54b4d4dd59dfcaeb034fa3b7c28d1e318ec6f627
malialiu <76604944+malialiu@users.noreply.github.com>
2023-02-27 06:37:10 -1000

    Camera perspective and Player controller

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,16 +6,37 @@ using System;
 public class CameraFollow : MonoBehaviour
 {
     public Transform targetObject;
-    public float offsetDistance = 5f; 
+    private Vector3 offsetDistance;
+
+    private float angle;
+    private float angle2;
     void Start()
     {
-
+	    offsetDistance = new Vector3(0.0f, 0.0f, -10f);
+	    angle = 25.0f;
+	    angle2 = 0.0f;
     }
-
     void Update()
     {
-        transform.position = targetObject.position - (targetObject.forward * offsetDistance);
-        transform.LookAt(targetObject);
-	transform.rotation = targetObject.rotation;
     }
+
+    void LateUpdate()
+    {
+        transform.position = targetObject.position + Quaternion.Euler(angle2, angle, 0.0f) * offsetDistance;
+
+        if (Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.S)) {
+            angle2 -= 40.0f * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.W)) {
+            angle2 += 40.0f * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.D)) {
+            angle -= 40.0f * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.A)) {
+            angle += 40.0f * Time.deltaTime;
+        }
+
+    
+        transform.LookAt(targetObject.position);
+    }
+
+
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,54 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using UnityEngine;
-using System;
+// https://www.youtube.com/watch?v=ExEJtw2mhR4
 
 public class PlayerController : MonoBehaviour
 {
-	public CharacterController characterController;
-	public float speed;
+    public CharacterController controller;
+    private float speed = 6;
+    private float rotationSpeed = 90;
+    private float gravity = -20f;
+    private float jumpSpeed = 10;
+    private float hInput;
+    private float vInput;
 
-	void Update()
-	{
-		Move();
-	}
+    Vector3 moveVelocity;
+    Vector3 turnVelocity;
 
-	private void Move()
-	{       
-		if (Input.GetKey (KeyCode.LeftArrow)) {
- 
-            		transform.RotateAround (transform.position, Vector3.up, -40.0f* Time.deltaTime);
-        	}
-                if (Input.GetKey (KeyCode.RightArrow)) {
- 
-            		transform.RotateAround (transform.position, Vector3.up, 40.0f* Time.deltaTime);
-        	}
-		if (Input.GetKey (KeyCode.UpArrow)) {
- 
-            		transform.RotateAround (transform.position, Vector3.right, 40.0f* Time.deltaTime);
-        	}
-                if (Input.GetKey (KeyCode.DownArrow)) {
- 
-            		transform.RotateAround (transform.position, Vector3.right, -40.0f* Time.deltaTime);
-        	}
-                if (Input.GetKey (KeyCode.W)) {
- 
-            		transform.position = transform.position + transform.forward * speed * Time.deltaTime;
-        	}
-		if (Input.GetKey (KeyCode.S)) {
- 
-            		transform.position = transform.position + transform.forward * -speed * Time.deltaTime;
-        	}
-                if (Input.GetKey (KeyCode.A)) {
- 
-            		transform.position = transform.position + transform.right * -speed * Time.deltaTime;
-        	}
-                if (Input.GetKey (KeyCode.D)) {
- 
-            		transform.position = transform.position + transform.right * speed * Time.deltaTime;
-        	}
-                
-		
-	}
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            vInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            vInput = -1f;
+        }
+        else
+        {
+            vInput = 0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            hInput = -0.5f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            hInput = 0.5f;
+        }
+        else
+        {
+            hInput = 0f;
+        }
+
+        if (controller.isGrounded)
+        {
+            moveVelocity = transform.forward * speed * vInput;
+            turnVelocity = transform.up * rotationSpeed * hInput;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveVelocity.y = jumpSpeed;
+            }
+        }
+        moveVelocity.y += gravity * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(turnVelocity * Time.deltaTime);
+        }
+        controller.Move(moveVelocity * Time.deltaTime);
+    }
 }
