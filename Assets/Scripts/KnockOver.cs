9654175a97bce0a41d71cdce0684c74661abbda8
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2023-03-20 23:06:37 -1000

    more knockable objects

---
+++ Assets/Scripts/KnockOver.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockOver: MonoBehaviour
+{
+    public Rigidbody item;
+    public float fallingThreshold = -1f;
+    public int pointsEarned = 1;
+    private bool isKnockedOver;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item.velocity.y < fallingThreshold)
+        {
+            isKnockedOver = true;
+        }
+        else
+        {
+            isKnockedOver = false;
+        }
+
+        if (isKnockedOver)
+        {
+            addScore();
+        }
+
+    }
+
+    private void addScore()
+    {
+        Debug.Log("Item is Knocked Over!");
+        ScoringSystem.theScore += pointsEarned;
+    }
+}
