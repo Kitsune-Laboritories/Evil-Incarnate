c0847c91ee25c849de9cda7fcfb4dc73dec613c0
Raymond Lai <rlai@hawaii.edu>
2023-04-04 00:47:46 -1000

    movement-test-V0.2

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -11,7 +11,7 @@ public class PlayerMove : MonoBehaviour
     public float jumpHeight = 2f;
 
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.1f;
     public LayerMask groundMask;
 
     Vector3 velocity;
