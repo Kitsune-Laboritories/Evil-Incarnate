77752b1d6ab716f369b42583d78ff3728eb967d5
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2023-04-18 01:19:43 -1000

    Drawer Animation and Reveal Key Script

---
+++ Assets/Scripts/DrawerAnimation.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerAnimation : MonoBehaviour
+{
+    private GameObject door;
+    public GameObject HiddenObject;
+    private Transform t;
+    private Transform player;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //Debug.Log(animator);
+
+    }
+
+    private void Awake()
+    {
+        t = this.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            GameObject[] openable = GameObject.FindGameObjectsWithTag("Openable");
+            float closestDistance = 1.5f;
+            GameObject closestObject = null;
+            foreach (GameObject obj in openable)
+            {
+                float distance = Vector3.Distance(player.position, obj.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestObject = obj;
+                }
+            }
+
+
+            if (closestObject != null)
+            {
+                door = closestObject;
+                //turn on animation
+                animator.SetBool("isOpen", true);
+                StartCoroutine(RevealCoroutine());
+                
+
+            }
+        }
+    }
+
+    IEnumerator RevealCoroutine()
+    {
+     
+        //yield on a new YieldInstruction that waits for # seconds.
+        yield return new WaitForSeconds(1);
+        HiddenObject.SetActive(true);
+
+    }
+
+}
