93130f8579bba58684cd7e7f648d1799adafb6d7
malialiu <76604944+malialiu@users.noreply.github.com>
2023-04-25 05:38:13 -1000

    Added more button sound

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -9,12 +9,18 @@ public class ScoreBoard : MonoBehaviour
     public Text output;
     private string playerName;
     public GameObject GameOverCanvas;
+    public AudioClip sound;
+    private GameObject m;
 
     // Start is called before the first frame update
     void Start()
     {
         playerName = PlayerName.playerName;
         GameOverCanvas.SetActive(true);
+        sound = (AudioClip) Resources.Load<AudioClip>("button");
+        m = new GameObject("Music");
+        m.AddComponent<AudioSource>();
+        m.GetComponent<AudioSource>().clip = sound;
     }
 
     // Update is called once per frame
@@ -32,22 +38,26 @@ public class ScoreBoard : MonoBehaviour
 
     public void MainMenuButton()
     {
+        m.GetComponent<AudioSource>().Play();
         SceneManager.LoadScene("MainMenu");
     }
 
     public void TryAgainButton()
     {
+        m.GetComponent<AudioSource>().Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void Quit()
     {
+        m.GetComponent<AudioSource>().Play();
         Application.Quit();
         Debug.Log("Player Has Quit The Game");
     }
 
     public void NextScene()
     {
+        m.GetComponent<AudioSource>().Play();
         SceneManager.LoadScene("City");
         ScoringSystem.keys = 0;
     }
