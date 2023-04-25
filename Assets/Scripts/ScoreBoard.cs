7295c7c3ce3446a1c38db6dfb18187f45db8f02b
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2023-04-24 18:26:04 -1000

    Scoreboard Update

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -7,47 +7,26 @@ using UnityEngine.SceneManagement;
 public class ScoreBoard : MonoBehaviour
 {
     public Text output;
-    private int newScore;
     private string playerName;
     public GameObject GameOverCanvas;
-    public GameObject Next;
-    public GameObject Try_Again;
 
     // Start is called before the first frame update
     void Start()
     {
-        newScore = ScoringSystem.theScore;
         playerName = PlayerName.playerName;
         GameOverCanvas.SetActive(true);
-        if (ScoringSystem.taskCity)
-        {
-            Next.SetActive(true);
-            Try_Again.SetActive(false);
-        }
-        else
-        {
-            Next.SetActive(false);
-            Try_Again.SetActive(true);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float timeValue = TimerCountdown.timeSpend;
-        if (timeValue > 0)
+        if (ScoringSystem.taskCity == true)
         {
-            int minutes = (int)timeValue / 60;
-            int seconds = (int)timeValue - (60 * minutes);
-            if(seconds < 10)
-            {
-                output.text = playerName + ": " + minutes + ":0" + seconds;
-            }
-            else
-            {
-                output.text = playerName + ": " + minutes + ":" + seconds;
-            }
-
+            output.text = "Congratulations! You made it home!";
+        }
+        else
+        {
+            output.text = "You Died Try Again";
         }
     }
 
